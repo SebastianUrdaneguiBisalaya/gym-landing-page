66e4319935b6284fb99c387f6f1fea369929e8e1
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-12-22 16:58:20 -0500

    refact: I abstracted some parts of code on components. For instance, membership and reason card.

---
+++ src/data/constants.ts
@@ -0,0 +1,120 @@
+const reasons = [
+  {
+    title: "15000 sq.m.",
+    icon: "./icon_line.png",
+    description:
+      "Un amplio gimnasio para practicar deporte: una distancia segura entre las máquinas de ejercicio",
+  },
+  {
+    title: "Más de 200 equipos",
+    icon: "./weights.png",
+    description:
+      "Sin colas en los simuladores. Equipamiento de primera calidad de LifeStyle, Hammer Strength, TechnoGym.",
+  },
+  {
+    title: "4 zonas de entrenamiento",
+    icon: "./training.png",
+    description:
+      "Desde cardio hasta funcional y ciclismo. Zona separada para boxeo y artes marciales mixtas",
+  },
+  {
+    title: "Funcionamiento las 24 horas",
+    icon: "./clock.png",
+    description:
+      "El gimnasio está abierto las 24 horas del día, también los fines de semana y feriados.",
+  },
+];
+
+const memberships = [
+  {
+    title: "Paquete 'Trial'",
+    price: "0",
+    benefits: [
+      "Acceso al gimnasio desde las 8:00 a.m. hasta las 2:00 p.m.",
+      "El entrenador a cargo te inducirá a las sesiones.",
+    ],
+  },
+  {
+    title: "Paquete 'Free Time'",
+    price: "49",
+    benefits: [
+      "La entrada del gimnasio es desde las 2:00 p.m. hasta las 4:00 p.m.",
+      "Sin suspensión de la membresía del gimnasio.",
+      "Acompañamiento del entrenador en las sesiones.",
+    ],
+  },
+  {
+    title: "Paquete '1 Mes 24/7'",
+    price: "85",
+    benefits: [
+      "Visita sin restricciones 24/7.",
+      "Acompañamiento del entrenador en las sesiones.",
+    ],
+  },
+  {
+    title: "Paquete 'Easy Start'",
+    price: "119",
+    benefits: [
+      "Visita sin restricciones 24/7.",
+      "Programa de entrenador personal.",
+      "Acceso a la aplicación del gimnasio.",
+      "Soporte del entrenador.",
+    ],
+  },
+  {
+    title: "Paquete 'In Shape (AM)'",
+    price: "165",
+    benefits: [
+      "Entrenamientos en mini grupos hasta las 2:00 p.m.",
+      "Sin suspensión de la membresía del gimnasio.",
+      "Clases con el entrenador en mini grupos 3 veces por semana.",
+      "Soporte del entrenador.",
+    ],
+  },
+  {
+    title: "Paquete 'In Shape (PM)'",
+    price: "195",
+    benefits: [
+      "Entrenamientos en mini grupos hasta las 5:00 p.m.",
+      "Sin suspensión de la membresía del gimnasio.",
+      "Clases con el entrenador en mini grupos 3 veces por semana.",
+      "Soporte del entrenador.",
+    ],
+  },
+];
+const trainers = [
+  {
+    name: "Victoria Rodriguez",
+    img: "./trainer-1.png",
+  },
+  {
+    name: "Elizabeth Smith",
+    img: "./trainer-2.png",
+  },
+  {
+    name: "Ivan Ivanov",
+    img: "./trainer-3.png",
+  },
+  {
+    name: "Carla Brown",
+    img: "./trainer-4.png",
+  },
+  {
+    name: "Antonio Perez",
+    img: "./trainer-5.png",
+  },
+  {
+    name: "Carlos Pastori",
+    img: "./trainer-6.png",
+  },
+  {
+    name: "Andrew Johnson",
+    img: "./trainer-7.png",
+  },
+  {
+    name: "Nicole Smith",
+    img: "./trainer-8.png",
+  },
+];
+
+export { reasons, memberships, trainers };
